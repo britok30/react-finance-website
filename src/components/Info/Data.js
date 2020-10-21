0e27ef4012b7585af08a90babf09ce82aa8e9cc1
Kelvin Brito <britok30@gmail.com>
2020-10-20 23:43:29 -0400

    info section styled + data

---
+++ src/components/Info/Data.js
@@ -0,0 +1,8 @@
+export const homeObjOne = {
+    is: 'about',
+    lightbg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees'
+};
---
+++ src/components/Info/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Button } from 'react-scroll';
+import {
+    InfoContainer,
+    InfoWrapper,
+    InfoRow,
+    Column1,
+    Column2,
+    TextWrapper,
+    TopLine,
+    Heading,
+    Subtitle,
+    BtnWrap,
+    ImgWrap,
+    Img,
+} from './InfoStyled';
+
+const Info = () => {
+    return (
+        <>
+            <InfoContainer>
+                <InfoWrapper>
+                    <InfoRow>
+                        <Column1>
+                            <TextWrapper>
+                                <TopLine>Top Line</TopLine>
+                                <Heading>Heading</Heading>
+                                <Subtitle>Subtitle</Subtitle>
+                                <BtnWrap>
+                                    <Button to="home" />
+                                </BtnWrap>
+                            </TextWrapper>
+                        </Column1>
+                        <Column2>
+                            <ImgWrap>
+                                <Img />
+                            </ImgWrap>
+                        </Column2>
+                    </InfoRow>
+                </InfoWrapper>
+            </InfoContainer>
+        </>
+    );
+};
+
+export default Info;
