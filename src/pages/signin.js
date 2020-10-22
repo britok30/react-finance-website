5f704d5a5c17d2cd31ca4b3b045de79c6d143ad6
Kelvin Brito <britok30@gmail.com>
2020-10-21 22:07:12 -0400

    route implementation

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-const signin = () => {
+const SignIn = () => {
     return (
         <div>
-            
+            <h1>SignIn</h1>
         </div>
-    )
-}
+    );
+};
 
-export default signin
+export default SignIn;
